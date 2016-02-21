cf7d240813b33fe43f69d6e34280720ede935dfc
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-02-21 13:19:07 -0800

    21 Feb: First version of script.

---
+++ generate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Posts to the DouchebagBrainwaves Tumblr account, much like my other
+automatically generated text accounts.
+"""
+
+import random
+
+import patrick_logger    # From https://github.com/patrick-brian-mooney/personal-library
+import social_media      # From https://github.com/patrick-brian-mooney/personal-library
+from social_media_auth import autolovecraft_client
+
+sys.path.append('/lovecraft/markov_sentence_generator/')
+from sentence_generator import *                            # https://github.com/patrick-brian-mooney/markov-sentence-generator
+
+if __name__ == "__main__":
+    pass
