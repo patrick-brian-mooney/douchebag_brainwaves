bce478a0dc71c33a9f64234e0fccefa883353907
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-02-21 16:13:48 -0800

    21 Feb 2016: First working version of script

--- generate.py
+++ generate.py
@@ -10,7 +10,7 @@ Github repo:
     https://github.com/patrick-brian-mooney/douchebag_brainwaves
 """
 
-import random
+import random, pprint, sys
 
 import patrick_logger    # From https://github.com/patrick-brian-mooney/personal-library
 import social_media      # From https://github.com/patrick-brian-mooney/personal-library
@@ -27,25 +27,39 @@ the_content = ''
 patrick_logger.verbosity_level = 2
 chains_file = '/DouchebagBrainwaves/essays/graham.3.pkl'
 
+the_markov_length, the_starts, the_mapping = read_chains(chains_file)
+
+
 def get_a_title():
     """Gets a title for this particular Paul Graham-style brainwave"""
-    return 'YOU GUYS I JUST THOUGHT OF THIS'
+    possible_starts = [
+      ['startups', 'VC', 'startup', 'VCs'],
+      ['LISP', 'Arc', 'programming', 'coding', 'ALGOL', 'Pascal', 'Python', 'FORTRAN']
+    ]
+    topical_starts = random.choice(possible_starts)
+
+    possible_titles = [
+      lambda: 'YOU GUYS I JUST THOUGHT OF THIS',
+      lambda: "REASONS WHY STARTUPS FAIL",
+      lambda: gen_text(the_mapping, the_starts, markov_length=the_markov_length, sentences_desired=1, paragraph_break_probability=0).upper().strip()[:-1],
+      lambda: gen_text(the_mapping, topical_starts, markov_length=the_markov_length, sentences_desired=1, paragraph_break_probability=0).upper().strip()[:-1]
+    ]
+    return random.choice(possible_titles)()
 
 def get_some_tags():
     """Gets some random tags to add to the standard tags."""
     return ""       # For now.
 
 if __name__ == "__main__":
-    patrick_logger.log_it("INFO: Everything's set up, let's go ...")
-    
-    the_markov_length, the_starts, the_mapping = read_chains(chains_file)
-    patrick_logger.log_it('INFO: chains read, moving on ...')
-    
+    patrick_logger.log_it("INFO: Everything's set up, let's go...")
     the_title = get_a_title()
     patrick_logger.log_it('INFO: Title is: %s' % the_title)
-    
+
     patrick_logger.log_it('INFO: Getting %d sentences.' % brainwave_length)
     the_brainwave = gen_text(the_mapping, the_starts, markov_length=the_markov_length, sentences_desired=brainwave_length, paragraph_break_probability=0)
-    patrick_logger.log_it("INFO: here's the brainwave:\n\n%s" %  the_brainwave, 2)
-    
+    patrick_logger.log_it("INFO: here's the brainwave:\n\n%s" % the_brainwave, 2)
+
     the_status = social_media.tumblr_text_post(douchebag_brainwaves_client, normal_tags + get_some_tags(), the_title, the_brainwave)
+    patrick_logger.log_it('INFO: the_status is: ' + pprint.pformat(the_status), 2)
+
+    patrick_logger.log_it('INFO: We\'re done', 2)
